3ec547f085ff668e6ea9c1ae5651dba85fdd4d01
sezim <osmsezimka@gmail.com>
2023-08-24 03:59:26 +0600

    use scanner

---
+++ src/w1/WhenWillIbeReadyfor.java
@@ -0,0 +1,12 @@
+package w1;
+
+import java.util.Scanner;
+
+public class WhenWillIbeReadyfor {
+    public static void main(String[] args) {
+        System.out.println("Please enter the number of weeks completed in the course:");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+    }
+}
