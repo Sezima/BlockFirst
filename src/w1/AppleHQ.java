9536a0b27654ca973b97e0d7f60a52c8c21eee22
sezim <osmsezimka@gmail.com>
2023-08-24 19:11:12 +0600

    use printf with %s %d

---
+++ src/w1/AppleHQ.java
@@ -0,0 +1,13 @@
+package w1;
+
+//"1 Infinite Loop, Cupertino, California 95014"
+
+public class AppleHQ {
+    public static void main(String[] args) {
+        String street = " infinite Loop";
+        String city  = "Cupertino";
+        String state = "california";
+        int zipCode = 95014;
+        System.out.printf("%s, %s, %s, %d", street, city, state, zipCode);
+    }
+}
