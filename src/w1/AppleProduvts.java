f50018c112d33e9ede7a057874b07ab5b35cc958
sezim <osmsezimka@gmail.com>
2023-08-24 19:23:00 +0600

    use printf and with ' in text

---
+++ src/w1/AppleProduvts.java
@@ -0,0 +1,12 @@
+package w1;
+
+public class AppleProduvts {
+    public static void main(String[] args) {
+        String company = "Apple";
+        String iphone14 = "Iphone14";
+        String  macbookPro = "Macbook Pro";
+        String appleWatch = "Apple Watch";
+        String ipad = "iPad";
+        System.out.printf("%s's product line includes: %s, %s, %s, $s.", company, iphone14, macbookPro, appleWatch, ipad);
+    }
+}
