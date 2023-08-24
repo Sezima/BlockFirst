8f3e891cb93d535f7a3a54fa39d2b7a30fd7a517
sezim <osmsezimka@gmail.com>
2023-08-24 19:52:20 +0600

    just use long data type

---
+++ src/w1/TeslaRevenue.java
@@ -0,0 +1,10 @@
+package w1;
+
+public class TeslaRevenue {
+    public static void main(String[] args) {
+        long revenue2021 = 53_823_000_000L;
+        long revenue2022 = 81_462_000_000L;
+        long totalRevenueForLast2years = revenue2021 + revenue2022;
+        System.out.println(totalRevenueForLast2years);
+    }
+}
