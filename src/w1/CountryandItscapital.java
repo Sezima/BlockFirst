1aaa35ebd0f2371275c345d76219874402c02af5
sezim <osmsezimka@gmail.com>
2023-08-26 21:59:16 +0600

    total2022Deliveries / numOfQuarters

---
+++ src/w1/CountryandItscapital.java
@@ -0,0 +1,13 @@
+package w1;
+
+public class CountryandItscapital {
+    public static void main(String[] args) {
+
+        int total2022Deliveries = 1310000;
+        int numOfQuarters = 4;
+        int averageDeliveryPerQuarter = total2022Deliveries / numOfQuarters;
+
+        System.out.println(averageDeliveryPerQuarter);
+
+    }
+}
